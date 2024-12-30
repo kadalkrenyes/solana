5245b40942c7c84686f748183f2227a917080db1
Kadal Krenyes <44739068+kadalkrenyes@users.noreply.github.com>
2024-12-31 01:31:45 +0700

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,23 +1,19 @@
 import { FC } from 'react';
 import Link from 'next/link';
-import { GithubIcon, TwitterIcon } from 'lucide-react';
+import { SyringeIcon, TwitterIcon } from 'lucide-react';
 export const Footer: FC = () => {
     return (
         <div className="flex">
             <footer className="border-t-2 border-[#141414] bg-black w-screen" >
                 <div className="py-4 flex justify-center">
-                    <div className="grid grid-cols-3">
-                        <Link href={"https://github.com/cryptoloutre/solana-tools"} target='_blank' className='flex mx-4'>
-                            <GithubIcon />
-                            <div className='ml-2'>Github</div>
-                        </Link>
-                        <Link href={"https://x.com/ToolsSolana"} target='_blank' className='flex mx-4'>
-                            <TwitterIcon />
+                    <div className="grid grid-cols-2">
+                        <Link href={"https://pump.fun/coin/"} target='_blank' className='flex mx-4'>
+                            <SyringeIcon />
                             <div className='ml-2'>ToolsSolana</div>
                         </Link>
-                        <Link href={"https://x.com/laloutre"} target='_blank' className='flex mx-4'>
+                        <Link href={"https://x.com/tbits_io"} target='_blank' className='flex mx-4'>
                             <TwitterIcon />
-                            <div className='ml-2'>LaLoutre</div>
+                            <div className='ml-2'>$TBITS</div>
                         </Link>
 
                     </div>
