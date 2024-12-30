0a4229be8f44d52d22b5dc307727e93d87345715
Kadal Krenyes <44739068+kadalkrenyes@users.noreply.github.com>
2024-12-31 02:35:52 +0700

    Update AppBar.tsx

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -18,7 +18,7 @@ export const AppBar: React.FC = () => {
   return (
     <div>
       {/* NavBar / Header */}
-      <div className="navbar flex h-20 flex-row md:mb-2 shadow-lg bg-portland text-neutral-content border-b border-zinc-600 bg-opacity-66">
+ <div className="navbar flex h-20 flex-row md:mb-2 shadow-lg" style={{ backgroundColor: '#E03A3E', borderBottom: '1px solid #4b4b4b' }}>
         <div className="navbar-start align-items-center">
           <div className="hidden sm:inline w-22 h-22 md:p-2 ml-10">
             <Link href="/" passHref className="font-bold text-2xl uppercase">
